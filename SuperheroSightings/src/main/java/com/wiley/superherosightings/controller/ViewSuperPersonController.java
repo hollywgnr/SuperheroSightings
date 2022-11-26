e8bb6dd51fea547e55882085fa478031112c2498
Tiara Ravix <tiararavix@gmail.com>
2022-11-26 17:45:45 -0500

    Trying to fix superperson individual view

---
+++ SuperheroSightings/src/main/java/com/wiley/superherosightings/controller/ViewSuperPersonController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.wiley.superherosightings.controller;
+
+import com.wiley.superherosightings.dao.SuperpersonDao;
+import com.wiley.superherosightings.dto.Superperson;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ *
+ * @author tiara
+ */
+@Controller
+public class ViewSuperPersonController {
+
+    @Autowired
+    SuperpersonDao superpersonDao;
+
+    @GetMapping("superperson")
+    public String displayAll(Model model) {
+        List<Superperson> allSups = superpersonDao.getAll();
+        model.addAttribute("superpersons", allSups);
+
+        return "superperson";
+    }
+
+    @GetMapping("superperson/{id}")
+    public String displaySuperperson(@PathVariable("superpersonId") int id, Model model) {
+        //model.addAttribute("superperson",superpersonDao.findById(id));
+        List<Superperson> allSups = superpersonDao.getAll();
+        for (Superperson sp : allSups) {
+            model.addAttribute("superperson", sp);
+        }
+        return "superperson";
+    }
+
+}
